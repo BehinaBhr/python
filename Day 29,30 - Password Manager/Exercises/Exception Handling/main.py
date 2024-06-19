3bc96e9f501f54a4ace60b96bcbb91682c6b86c4
BehinaBhr <be.bhr77@gmail.com>
2024-06-18 20:42:08 -0700

    Update Exeption Handeling Excersies of Day 29, 30 project

--- Day 29,30 - Password Manager/Exercises/Exception Handling/main.py
+++ Day 29,30 - Password Manager/Exercises/Exception Handling/main.py
@@ -1,18 +1,26 @@
 import pandas
 
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_dict = {row.letter: row.code for (index, row) in nato_data_frame.iterrows()}
 
-phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
 
-no_error = True
-
-while no_error:
-    word = input("Enter a word: ").upper()
+def phonetic_generator():
+    name = input("Enter a name: ").upper()
     try:
-        output_list = [phonetic_dict[letter] for letter in word]
-    except KeyError:
-        print("Sorry, only letters in the alphabet please.")
+        code_words = [nato_dict[letter] for letter in name]
+    except:
+        print(" Sorry, Only letter is acceptable.")
+        phonetic_generator()
     else:
-        print(output_list)
-        no_error = False
+        print(code_words)
+
+
+phonetic_generator()
+split_name = [letter for letter in name]
+print(split_name)
+
+your_dict = {letter: nato_dict[letter] for letter in split_name}
+print(your_dict)
+
+another_your_dict = {letter: code for (letter, code) in nato_dict.items() if letter in split_name}
+print(another_your_dict)
