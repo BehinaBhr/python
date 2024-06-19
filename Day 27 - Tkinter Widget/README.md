289de0772d997116b3bf165528054fd31e630b83
BehinaBhr <be.bhr77@gmail.com>
2024-06-18 20:25:29 -0700

    Implement Day 27 project

---
+++ Day 27 - Tkinter Widget/README.md
@@ -0,0 +1,37 @@
+# Day 27: Project - Tkinter widget__Unit Converter Program
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+
+## Overview
+
+### The challenge
+
+- Create a GUI that converts miles to kilometers 
+
+### Links
+
+- Solution URL: [Day 27](https://replit.com/@appbrewery/day-27-end)
+
+## My process
+
+### Built with
+
+- Python
+- Tkinter
+
+### What I learned
+- Graphical User Interfaces with Tkinter 
+  - Tkinter Widgets: Buttons, Entry, Radiobuttons, Scales and others
+  - Tkinter Methods: pack(), place() and grid()
+- Advanced Functions (Default Arguments):
+  - *Args
+  - **Kwargs
+
+ 
---
+++ Day 27 - Tkinter Widget/main.py
@@ -0,0 +1,124 @@
+# TODO: Show "Button Got Clicked" on my_label when the button get's clicked.
+
+# def button_clicked():
+#     # print("I’m Hear")
+#     my_label.config(text="Button Got Clicked")
+#
+#
+# button = Button(text="click me", command=button_clicked)
+# button.pack()
+
+# TODO: get whatever is written as input to be the text in the label the moment when this button clicked
+
+# box = Entry(width=10)
+# box.pack()
+# def button_clicked_after_box():
+#     new_text = box.get()
+#     my_label.config(text=new_text)
+#
+# button = Button(text="click me", command=button_clicked_after_box)
+# button.pack()
+
+from tkinter import *
+
+# Creating a new window and configurations
+window = Tk()
+window.title("MY First GUI")
+window.minsize(width=500, height=500)
+# Add padding around window
+window.config(padx=100, pady=200)
+
+# Labels
+label = Label(text="This is old text")
+label.config(text="This is new text")
+# my_label["text"] = "This is new text"
+label.pack()
+
+
+# Buttons
+def action():
+    print("Do something")
+
+
+# calls action() when pressed
+button = Button(text="Click Me", command=action)
+button.pack()
+
+# Entries
+entry = Entry(width=30)
+# Add some text to begin with
+entry.insert(END, string="Some text to begin with.")
+# Gets text in entry
+print(entry.get())
+entry.pack()
+
+# Text
+text = Text(height=5, width=30)
+# Puts cursor in textbox.
+text.focus()
+# Adds some text to begin with.
+text.insert(END, "Example of multi-line text entry.")
+# Get's current value in textbox at line 1, character 0
+print(text.get("1.0", END))
+text.pack()
+
+
+# Spinbox
+def spinbox_used():
+    # gets the current value in spinbox.
+    print(spinbox.get())
+
+
+spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+spinbox.pack()
+
+
+# Scale
+# Called with current scale value.
+def scale_used(value):
+    print(value)
+
+
+scale = Scale(from_=0, to=100, command=scale_used)
+scale.pack()
+
+
+# Checkbutton
+def checkbutton_used():
+    # Prints 1 if On button checked, otherwise 0.
+    print(checked_state.get())
+
+
+# variable to hold on to checked state, 0 is off, 1 is on.
+checked_state = IntVar()
+checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+checked_state.get()
+checkbutton.pack()
+
+
+# Radiobutton
+def radio_used():
+    print(radio_state.get())
+
+
+# Variable to hold on to which radio button value is checked.
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
+
+# Listbox
+def listbox_used(event):
+    # Gets current selection from listbox
+    print(listbox.get(listbox.curselection()))
+
+
+listbox = Listbox(height=4)
+fruits = ["Apple", "Pear", "Orange", "Banana"]
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+listbox.bind("<<ListboxSelect>>", listbox_used)
+listbox.pack()
+window.mainloop()
